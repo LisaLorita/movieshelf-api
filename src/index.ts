01430c310e6eb6b485b71cac165131e0f851772a
Alba Martin <alba.martingz@gmail.com>
2025-04-22 16:57:59 +0200

    feat(database): add connectMongoDB function and config db conection  before starting server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import app from './app';
+import connectMongoDB from './config/db';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+  await connectMongoDB();
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+  }
+  catch (error) {
+    console.error('Error starting server:', error);
+    process.exit(1);
+  }
+};
+startServer();
