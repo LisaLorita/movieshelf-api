5e421bff2a600f27f157f666e6bee8bb316b39e1
Alba Martin <alba.martingz@gmail.com>
2025-04-24 23:08:49 +0200

    feat(users): add router config with post request

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import usersRoutes from './modules/users/routes/users.routes';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use('/api', usersRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   console.log('Hello World!');
