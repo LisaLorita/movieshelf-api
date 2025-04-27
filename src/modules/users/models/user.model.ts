bc17e7006000074b640d2534ecd1691a6c06dc52
Alba Martin <alba.martingz@gmail.com>
2025-04-27 23:17:15 +0200

    feat(users): add hash password middlewear

--- src/modules/users/models/user.model.ts
+++ src/modules/users/models/user.model.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs"; 
 
-const userSchema = new mongoose.Schema({
+
+const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -19,6 +21,24 @@ const userSchema = new mongoose.Schema({
   timestamps: true
 });
 
-const User = mongoose.model("User", userSchema);
+UserSchema.pre('save', async function(next) {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+    user.password = hashedPassword;
+    next();
+  } catch (error) {
+    if (error instanceof Error) {
+      next(new Error(`Error hashing password: ${error.message}`));
+    }
+  }
+});
+
+const User = mongoose.model("User", UserSchema);
 
 export default User;
