678b48781a8e86994f4d342dd3650bfe235c5016
Alba Martin <alba.martingz@gmail.com>
2025-06-24 11:08:25 +0200

    feat(users): add create-user validation middleware

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import { createUser } from '../controllers/users.controller';
+import { validateWithJoi } from '../validation/joi.validation.middleware';
+import { createUserSchema } from '../validation/create-user.joi.schema';
 
 const router = Router();
   
-router.post('/users', createUser);
+router.post('/users', 
+  validateWithJoi(createUserSchema),
+  createUser);
 
 export default router; 
